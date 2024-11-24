141778df4797621e3c5e3a729b3c6cec152c5e2c
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-24 12:01:54 +0500

    hu

---
+++ boolean10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace boolean10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = int.Parse(ReadLine()); 
+           int b = int.Parse(ReadLine());
+           bool result = a %2 == 0 || b % 2 == 0;
+           WriteLine(result);
+        }
+    }
+}
